259b54419ae2b91ef7032f1dac54c80d82ec330f
hendisantika <hendisantika@yahoo.co.id>
2024-02-26 06:48:43 +0700

    Add deploy.sh 🫸🌀✏️📗🐧🐳

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+start=$(date +"%s")
+
+ssh -p ${SERVER_PORT} ${SERVER_USER}@${SERVER_HOST} -i key.txt -t -t -o StrictHostKeyChecking=no << 'ENDSSH'
+docker pull hendisantika/nuxt-blog:latest
+
+CONTAINER_NAME=nuxt-blog
+if [ "$(docker ps -qa -f name=$CONTAINER_NAME)" ]; then
+    if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
+        echo "Container is running -> stopping it..."
+        docker stop $CONTAINER_NAME;
+    fi
+fi
+
+docker run -d --rm -p 3000:3000 --name $CONTAINER_NAME hendisantika/nuxt-blog:latest
+
+exit
+ENDSSH
+
+if [ $? -eq 0 ]; then
+  exit 0
+else
+  exit 1
+fi
+
+end=$(date +"%s")
+
+diff=$(($end - $start))
+
+echo "Deployed in : ${diff}s"
